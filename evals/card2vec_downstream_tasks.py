48a9ce3ca792f383b1a1f7e25a6f7e82d5dddbb1
Jordan Reef <jreef3@gatech.edu>
2023-12-12 02:12:43 -0500

    Add k-means clustering to the draft picker. Add more context to some of the draft puzzles.

--- evals/card2vec_downstream_tasks.py
+++ evals/card2vec_downstream_tasks.py
@@ -11,8 +11,12 @@ import matplotlib.pyplot as plt
 from itertools import combinations
 
 from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.metrics import pairwise_distances
+from sklearn.metrics.pairwise import cosine_similarity
 
 from data_preprocessing.seventeen_lands_preprocessor import NAME_2_ID, ID_2_NAME
+
 
 class Card2VecEmbeddingEval(object):
     """
@@ -189,7 +193,8 @@ class Card2VecEmbeddingEval(object):
 
         plt.close()
 
-    def draft_pick(self, context, choices, winrates, wr_lower=0.2, wr_upper=0.7):
+    def draft_pick(self, context, choices, winrates, wr_lower=0.2, wr_upper=0.7,
+                   eval_type='additive', cluster_algo='centroid', k=None):
         """
         Make a draft pick! Chooses a card from among choices that maximizes the cosine similarity to a centroid
         calculated from all currently selected cards (the 'context'). These similarities are weighted by the overall
@@ -205,29 +210,78 @@ class Card2VecEmbeddingEval(object):
             wr_lower (float) : Lower bound of the winrate normalization range
             wr_upper (float) : Upper bound of the winrate normalization range
 
+            eval_type (str) : One of { additive, multiplicative, no_embed, no_winrate }
+                              Determines the calculation used to finally make the draft pick:
+                                - additive       : adds the similarity metric to the winrate metric
+                                - multiplicative : multiplies the similarity metric by the winrate metric
+                                - no_embed       : picks based on raw winrate statistics
+                                - no_winrate     : picks based only on the embedding similarity
+
+            cluster_algo (str) : One of { centroid, k_means, db_scan }
+                                 Which algorithm to use when generating clusters. Either naiive centroid, k_means, db_scan
+
+            k (int) : Choice of k (if using k_means cluster_algo)
+
         Return:
             choice (int) : index of chosen card within the embedding
         """
+        if cluster_algo == 'k_means':
+            if k is None:
+                raise ValueError("Need to specify value for param 'k' when using cluster_algo == 'k_means'")
+
+            # Don't use more clusters than there are context vectors
+            if k > len(context):
+                k = len(context)
+
         ctx_idxs = [self.card_names[0][name] for name in context]
         choice_idxs = [self.card_names[0][name] for name in choices]
 
-        # Get the embeddings for each card in the context
-        ctx_embeddings = torch.stack([F.embedding(torch.tensor(idx).to(self.device), weight=self.embed_weights)
-                                      for idx in ctx_idxs]).to(self.device)
+        sims = None  # similarities determined by clustering algorithm
+        if cluster_algo == 'centroid':  # naiive centroid ______________________________________________________________
+            # Single centroid of context
+            ctx_embeddings = torch.stack([F.embedding(torch.tensor(idx).to(self.device), weight=self.embed_weights)
+                                          for idx in ctx_idxs]).to(self.device)
+            ctx_centroid = torch.mean(ctx_embeddings, dim=0)
 
-        # Calculate context centroid
-        ctx_centroid = torch.mean(ctx_embeddings, dim=0)
+            choice_embeddings = [F.embedding(torch.tensor(idx).to(self.device), weight=self.embed_weights).to(self.device)
+                                 for idx in choice_idxs]
 
-        # Get the embeddings for each card in the choices
-        choice_embeddings = [F.embedding(torch.tensor(idx).to(self.device), weight=self.embed_weights)
-                             for idx in choice_idxs]
+            # Get the similarity between the context centroid and each choice -- these are logits
+            sims = torch.tensor([F.cosine_similarity(ctx_centroid, ce, dim=0).item()
+                                 for ce in choice_embeddings]).to(self.device)
 
-        # Get the similarity between the context centroid and each choice -- these are logits
-        sims = torch.tensor([F.cosine_similarity(ctx_centroid, ce, dim=0).item()
-                             for ce in choice_embeddings]).to(self.device)
+        elif cluster_algo == 'k_means':  # k_means _____________________________________________________________________
+            # K-Means on context
+            ctx_embeddings = torch.stack([F.embedding(torch.tensor(idx).to(self.device), weight=self.embed_weights)
+                                          for idx in ctx_idxs]).cpu().numpy()
+
+            kmeans = KMeans(n_clusters=k, random_state=2024, n_init=20, algorithm='elkan')
+            kmeans.fit(ctx_embeddings)
+
+            # Find the largest cluster
+            biggest_cluster_idx = np.argmax(np.bincount(kmeans.labels_))
+            big_cluster = kmeans.cluster_centers_[biggest_cluster_idx]
+
+            # Get the embeddings for each card in the choices
+            choice_embeddings = [F.embedding(torch.tensor(idx).to(self.device), weight=self.embed_weights).cpu().numpy()
+                                 for idx in choice_idxs]
+
+            # # For each choice, get its highest similarity to any of the k centroids -- these are logits
+            # sims = np.min(pairwise_distances(choice_embeddings, kmeans.cluster_centers_, metric='cosine'), axis=1)
+
+            # Cosine similarity
+            sims = [np.dot(ce, big_cluster) / (np.linalg.norm(ce) * np.linalg.norm(big_cluster))
+                    for ce in choice_embeddings]
+            sims = torch.tensor(sims).to(self.device)  # Pass back to torch
+
+            print("Break")
+        else:
+            raise ValueError("Invalid cluster_algo passed to draft_pick()")
+
+        # ______________________________________________________________________________________________________________
 
         # Take a Softmax over the similarities
-        sims = F.softmax(sims)
+        sims = F.softmax(sims, dim=0)
 
         # Preprocess card winrates
         _wrs = [(self.card_names[NAME_2_ID][tup[0]], tup[1]) for tup in winrates]
@@ -249,7 +303,16 @@ class Card2VecEmbeddingEval(object):
         wr_min = torch.min(wrs)
         wrs = wr_lower + (wrs - wr_min) * (wr_upper - wr_lower) / (wr_max - wr_min)
 
-        normalized_sims = torch.tensor([sims[i] * wrs[choice_idxs[i]] for i in range(len(choices))]).to(self.device)
+        if eval_type == "additive":
+            normalized_sims = torch.tensor([sims[i] + wrs[choice_idxs[i]] for i in range(len(choices))]).to(self.device)
+        elif eval_type == "multiplicative":
+            normalized_sims = torch.tensor([sims[i] * wrs[choice_idxs[i]] for i in range(len(choices))]).to(self.device)
+        elif eval_type == "no_embed":
+            normalized_sims = torch.tensor([wrs[choice_idxs[i]] for i in range(len(choices))]).to(self.device)
+        elif eval_type == "no_winrate":
+            normalized_sims = sims
+        else:
+            raise ValueError('Invalid eval_type passed to Card2VecEmbeddingEval.draft_pick()')
 
         # Return the embedding index of the best pick
         return choice_idxs[torch.argmax(normalized_sims).item()]
--- evals/eval_puzzles.py
+++ evals/eval_puzzles.py
@@ -4,7 +4,7 @@ Static data needed for doing some downstream evaluations / puzzles on card2vec a
 
 # region WOE t-SNE Clusters
 
-staples_white = ["Plains", "Besotted Knight", "Cooped Up", "Hopeful Vigil", "The Princess takes Flight", "Stockpiling Celebrant",
+staples_white = ["Plains", "Besotted Knight", "Cooped Up", "Hopeful Vigil", "The Princess Takes Flight", "Stockpiling Celebrant",
                  "Spellbook Vendor", "Werefox Bodyguard", "Cursed Courtier", "Kellan's Lightblades", "Archon's Glory"]
 
 staples_blue = ["Island", "Aquatic Alchemist", "Archive Dragon", "Bitter Chill", "Horned Loch-Whale", "Ice Out",
@@ -45,84 +45,153 @@ WOE_tsne_clusters = {
 
 # endregion
 
-# region Draft Pick Puzzles
+# region WOE Draft Pick Puzzles
 
 # Pick the only choice that is the correct color -- should select "Hopeful Vigil"
-easy_pick_1 = {
-    'context': ["Spellbook Vendor", "Archon's Glory", "Stockpiling Celebrant"],
-    'choices': ["Hopeful Vigil", "Into the Fae Court", "Barrow Naughty", "Frantic Firebolt", "Territorial Witchstalker"]
+woe_easy_pick_1 = {
+    'context': ["Besotted Knight", "Cooped Up", "The Princess Takes Flight", "Stockpiling Celebrant",
+                 "Spellbook Vendor", "Werefox Bodyguard", "Cursed Courtier", "Kellan's Lightblades", "Archon's Glory"],
+    'choices': ["Hopeful Vigil", "Into the Fae Court", "Barrow Naughty", "Frantic Firebolt", "Royal Treatment"],
+    'solutions': ["Hopeful Vigil"]
 }
 
-# Pick the only choice that is the correct color -- should select "Gruff Triplets"
-easy_pick_2 = {
-    'context': ["Royal Treatment", "Hollow Scavenger", "Rootrider Faun"],
-    'choices': ["Gruff Triplets", "Ruby, Daring Tracker", "Syr Armont, the Redeemer", "Rat Out", "Ice Out"]
+# Pick the only choice that is the correct color -- should select "Hamlet Glutton"
+woe_easy_pick_2 = {
+    'context': ["Agatha's Champion", "Brave the Wilds", "Curse of the Werefox", "Ferocious Werefox", "Graceful Takedown",
+                 "Gruff Triplets", "Hollow Scavenger", "Rootrider Faun", "Royal Treatment"],
+    'choices': ["Hamlet Glutton", "Ruby, Daring Tracker", "Syr Armont, the Redeemer", "Sweettooth Witch", "Ice Out"],
+    'solutions': ["Hamlet Glutton"]
 }
 
-# Pick the signpost uncommon for my color pair (UR) -- should select "Johann, Apprentice Sorcerer"
-easy_pick_3 = {
-    'context': ["Torch the Tower", "Flick a Coin", "Picklock Prankster", "Ice Out"],
-    'choices': ["Johann, Apprentice Sorcerer", "Return Triumphant", "Mintstrosity", "Ferocious Werefox", "Gingerbrute"]
+# Pick the signpost uncommon for my color pair (WR) -- should select "Ash, Party Crasher"
+woe_easy_pick_3 = {
+    'context': ["Armory Mice", "Cheeky House-Mouse", "Hopeful Vigil", "Return Triumphant", "Stockpiling Celebrant",
+                "Belligerent of the Ball", "Embereth Veteran", "Harried Spearguard", "Gingerbrute"],
+    'choices': ["Ash, Party Crasher", "Ice Out", "Mintstrosity", "Up the Beanstalk", "Gingerbrute"],
+    'solutions': ["Ash, Party Crasher"]
 }
 
 # Pick a card in the correct strategy (WR aggro) -- some choices are in the correct colors but not the right strategy
 # should select either "Grand Ball Guest" or "Armory Mice"
-medium_pick_1 = {
-    'context': ["Ash, Party Crasher", "Cheeky House Mouse", "Return Triumphant", "Belligerent of the Ball", "Embreth Veteran"],
+woe_medium_pick_1 = {
+    'context': ["Cheeky House-Mouse", "Hopeful Vigil", "Return Triumphant", "Stockpiling Celebrant",
+                "Belligerent of the Ball", "Embereth Veteran", "Harried Spearguard", "Ash, Party Crasher", "Gingerbrute"],
     'choices': ["Grand Ball Guest", "Armory Mice",
                 "Unruly Catapult", "Knight of Doves",
-                "Quick Study", "Scream Puff", "Brave the Wilds", "Prophetic Prism"]
+                "Ice Out", "Taken by Nightmares", "Hamlet Glutton", "Feral Encounter"],
+    'solutions': ["Grand Ball Guest", "Armory Mice"]
 }
 
 # Pick the best card given no strong attachment to any colors / strategies -- should select "Imodane's Recruiter"
-medium_pick_2 = {
+woe_medium_pick_2 = {
     'context': ["The Princess Takes Flight", "Bitter Chill", "The Witch's Vanity", "Torch the Tower", "Rootrider Faun"],
     'choices': ["Imodane's Recruiter",
-                "Besotted Knight", "Spell Stutter", "Barrow Naughty", "Gnawing Crescendo", "Hamlet Glutton"]
+                "Besotted Knight", "Spell Stutter", "Barrow Naughty", "Gnawing Crescendo", "Hamlet Glutton"],
+    'solutions': ["Imodane's Recruiter"]
 }
 
-# Avoid being confounded by off colors in context, i.e. stick with your best color (G)
-# should select "Tough Cookie"
-medium_pick_3 = {
+# Avoid being confounded by off colors in context, i.e. stick with your best color (Green)
+# should select "Agatha's Champion"
+woe_medium_pick_3 = {
     'context': ["Welcome to Sweettooth", "Hollow Scavenger", "Hamlet Glutton",
-                "Torch the Tower", "Voracious Vermin", "Living Lectern"],
-    'choices': ["Tough Cookie",
-                "Cut In", "Candy Grapple", "Ice Out", "Stockpiling Celebrant", "Totentanz, Swarm Piper", "Restless Bivouac"]
+                "Torch the Tower", "Voracious Vermin"],
+    'choices': ["Agatha's Champion",
+                "Cut In", "Candy Grapple", "Ice Out", "Stockpiling Celebrant", "The Goose Mother", "Restless Bivouac"],
+    'solutions': ["Agatha's Champion"]
 }
 
 # Pick the best card for in my strategy (WU control) given no clear color signal
 # should select 'Expel the Interlopers'
-hard_pick_1 = {
+woe_hard_pick_1 = {
     'context': ["Farsight Ritual", "Ice Out", "Johann's Stopgap", "Bitter Chill", "Collector's Vault"],
     'choices': ["Expel the Interlopers",
-                "Armory Mice", "Tangled Colony", "Mintstrosity", "Gnawing Crescendo", "Harried Spearguard", "Hollow Scavenger", "Leaping Ambush", "Troyan, Gutsy Explorer"]
+                "Armory Mice", "Tangled Colony", "Mintstrosity", "Goblin Bombardment", "Harried Spearguard", "Stormkeld Vanguard", "Leaping Ambush", "Troyan, Gutsy Explorer"],
+    'solutions': ["Expel the Interlopers"]
 }
 
 # Select a crucial strategy piece, avoid good on-color staples
 # should select 'Season of Growth'
-hard_pick_2 = {
+woe_hard_pick_2 = {
     'context': ["Monstrous Rage", "Cut In", "Witch's Mark", "Curse of the Werefox", "Royal Treatment"],
     'choices': ["Season of Growth",
                 "Torch the Tower", "Grand Ball Guest", "Tough Cookie", "Rootrider Faun", "Ruby, Daring Tracker",
-                "Regal Bunnicorn", "Sleep-Cursed Faerie", "Candy Grapple"]
+                "Regal Bunnicorn", "Sleep-Cursed Faerie", "Candy Grapple"],
+    'solutions': ["Season of Growth"]
 }
 
 # Pack 2 Pick 1 -- Generally tough pick, relatively large context -- strategy is (BR rat aggro)
 # Choice contains some of the best cards in the set that do not share our color, and decent cards that share our color
 # (but not our strategy)
 # should select one of: {"Totentanz, Swarm Piper", "Torch the Tower", "Candy Grapple"}
-hard_pick_3 = {
+woe_hard_pick_3 = {
     'context': ['Redcap Gutter-Dweller', 'Tangled Colony', 'Rat Out', 'Ratcatcher Trainee', "Lord Skitter's Butcher",
                 "Voracious Vermin", "Edgewall Pack", "Tattered Ratter", "Harried Spearguard", "Bespoke Battlegarb",
                 "Edgewall Inn", "Candy Trail", "Misleading Motes", "Slumbering Keepguard"],
     'choices': ["Totentanz, Swarm Piper", "Torch the Tower", "Candy Grapple",
                 "Gruff Triplets", "Greta, Sweettooth Scourge", "Hopeful Vigil", "Archive Dragon",
                 "Faerie Dreamthief", "Barrow Naughty", "Grabby Giant", "Hearth Elemental",
-                "Gingerbrute", "Prophetic Prism", "Scarecrow Guide", "Crystal Grotto"]
+                "Gingerbrute", "Prophetic Prism", "Scarecrow Guide", "Crystal Grotto"],
+    'solutions': ["Totentanz, Swarm Piper", "Torch the Tower", "Candy Grapple"]
 }
 
-WOE_draft_puzzles = [easy_pick_1, easy_pick_2, easy_pick_3,
-                     medium_pick_1, medium_pick_2, medium_pick_3,
-                     hard_pick_1, hard_pick_2, hard_pick_3]
+WOE_draft_puzzles = [woe_easy_pick_1, woe_easy_pick_2, woe_easy_pick_3,
+                     woe_medium_pick_1, woe_medium_pick_2, woe_medium_pick_3,
+                     woe_hard_pick_1, woe_hard_pick_2, woe_hard_pick_3]
+
+# endregion
+
+# region LTR Draft Pick Puzzles
+
+ltr_easy_pick_1 = {
+    'context': ["Rohirrim Lancer"],
+    'choices': ["Relentless Rohirrim", "Revive the Shire"],
+    'solutions': ["Relentless Rohirrm"]
+}
+
+ltr_easy_pick_2 = {
+    'context': ["Dunland Crebain"],
+    'choices': ["Easterling Vanguard", "Hithlain Knots"],
+    'solutions': ["Easterling Vanguard"]
+}
+
+ltr_easy_pick_3 = {
+    'context': ["Errand-Rider of Gondor"],
+    'choices': ["Flowering of the White Tree", "Orcish Medicine"],
+    'solutions': ["Flowering of the White Tree"]
+}
+
+ltr_medium_pick_1 = {
+    'context': ["Dunland Crebain"],
+    'choices': ["Easterling Vanguard", "Ithilien Kingfisher"],
+    'solutions': ["Easterling Vanguard"]
+}
+
+ltr_medium_pick_2 = {
+    'context': ["Peregrine Took"],
+    'choices': ["Shire Shirriff", "Mirkwood Spider"],
+    'solutions': ["Shire Shirriff"]
+}
+
+ltr_medium_pick_3 = {
+    'context': ["Smite the Deathless"],
+    'choices': ["Ranger's Firebrand", "Stew the Coneys"],
+    'solutions': ["Ranger's Firebrand"]
+}
+
+ltr_hard_pick_1 = {
+    'context': ["Old Man Willow"],
+    'choices': ["Entish Restoration", "Voracious Fell Beast"],
+    'solutions': ["Entish Restoration"]
+}
+
+ltr_hard_pick_2 = {
+    'context': ["Rohirrim Lancer"],
+    'choices': ["Horn of Gondor", "Foray of Orcs"],
+    'solutions': ["Horn of Gondor"]
+}
+
+LTR_draft_puzzles = [ltr_easy_pick_1, ltr_easy_pick_2, ltr_easy_pick_3,
+                     ltr_medium_pick_1, ltr_medium_pick_2, ltr_medium_pick_3,
+                     ltr_hard_pick_1, ltr_hard_pick_2]
 
 # endregion
